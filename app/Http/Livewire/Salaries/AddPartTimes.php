d451b7f7b3fc4b986b91a50f281715db02ba547c
Mhmad-jamal <qusairayan8@gmail.com>
2023-09-28 17:41:40 +0300

    sdfgfas

--- app/Http/Livewire/Salaries/AddPartTimes.php
+++ app/Http/Livewire/Salaries/AddPartTimes.php
@@ -62,9 +62,9 @@ class AddPartTimes extends Component
         $this->validate([
             'employee' => 'required|exists:users,id',
             'from' => 'required|date',
-           
+
         ]);
-       
+
 
 
         if ($this->employee) {
@@ -72,12 +72,11 @@ class AddPartTimes extends Component
             $this->noSalary = true;
 
             if ($salary) {
-                if($salary->salary &&  $salary->part_time){ 
+                if ($salary->salary &&  $salary->part_time) {
                     $this->period = $salary->part_time;
                     $this->salary = $salary->salary;
-                    $this->noSalary = false;}
-
-               
+                    $this->noSalary = false;
+                }
             } else {
                 $this->period = '';
                 $this->noSalary = true;
@@ -87,91 +86,84 @@ class AddPartTimes extends Component
 
 
 
-        
+
 
         // Assuming you have two date values as Carbon objects
-if ($this->from && $this->to ) {
-    $this->dateSet=false;
+        if ($this->from && $this->to) {
+            $this->dateSet = false;
 
 
 
-        $from = Carbon::parse($this->from);
-        $to = Carbon::parse($this->to);
+            $from = Carbon::parse($this->from);
+            $to = Carbon::parse($this->to);
 
-If($to && $to <= $from ){
-            $this->date_incorrect = true;
-            return false;
+            if ($to && $to <= $from) {
+                $this->date_incorrect = true;
+                return false;
+            } else {
+                $this->date_incorrect = false;
+            }
+
+            $daysDifference = $from->diffInDays($to);
+
+
+            if ($this->period == 'daily') {
+                $this->total =  round($this->salary * $daysDifference, 1);
+            } else if ($this->period == 'weekly') {
+                $this->total =  round($this->salary / 7 * $daysDifference, 1);
+            } else if ($this->period == 'monthly') {
+                $this->total = round($this->salary / 30 * $daysDifference / 30, 1);
+            }
         }
-        else{
-            $this->date_incorrect = false;
-        }
-
-        $daysDifference = $from->diffInDays($to);
-
-
-        if($this->period == 'daily'){
-            $this->total=  round($this->salary * $daysDifference,1);
-          }
-          else if($this->period == 'weekly'){
-            $this->total=  round( $this->salary/7 * $daysDifference,1);
-          } else if($this->period == 'monthly'){
-            $this->total= round($this->salary/30 * $daysDifference/30,1);
-          }
-
-}
     }
 
 
     public function add()
     {
 
-
         $this->validate([
             'employee' => 'required|exists:users,id',
             'from' => 'required',
-            
+
         ]);
 
         $from = Carbon::parse($this->from);
         $to = Carbon::parse($this->to);
 
-If($to && $to <= $from ){
+        if ($to && $to <= $from) {
             $this->date_incorrect = true;
             return false;
-        }
-        else{
+        } else {
             $this->date_incorrect = false;
-            
         }
 
 
         $check = PartTime::where('user_id', '=', $this->employee)
-        ->where(function ($query) {
-            $query->whereBetween('from', [$this->from, $this->to])
-                ->orWhereBetween('to', [$this->from, $this->to]);
-        })
-        ->first();
+            ->where(function ($query) {
+                $query->whereBetween('from', [$this->from, $this->to])
+                    ->orWhereBetween('to', [$this->from, $this->to]);
+            })
+            ->first();
 
         if ($check) {
 
-            $this->dateSet=true;
+            $this->dateSet = true;
             return false;
-                }
-                $status=0;
-                if($this->from && $this->to){
-                    $status=1;
+        }
+        $status = 0;
+        if ($this->from && $this->to) {
+            $status = 1;
+        }
 
-                }
 
-                
 
         $PartTime = PartTime::create([
             'user_id' => $this->employee,
             'from' => $this->from,
-            'to' => $this->to,
+            'to' => $this->to==''?null:$this->to,
             'status' => $status,
             'amount' => $this->total,
-           
+
 
         ]);
 
@@ -211,6 +203,6 @@ If($to && $to <= $from ){
 
         $partime = $partimeQuery->get();
 
-        return view('livewire.salaries.addPartTime', compact('departments','companies', 'partime'));
+        return view('livewire.salaries.addPartTime', compact('departments', 'companies', 'partime'));
     }
 }
