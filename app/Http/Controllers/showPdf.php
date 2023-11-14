36e89cbc062a0870e3bf3d3e0edbf19afc9985b1
MarwanAlbkirat <marwan19951980@gmail.com>
2023-11-14 14:15:03 +0300

    updateSalary

--- app/Http/Controllers/showPdf.php
+++ app/Http/Controllers/showPdf.php
@@ -1,17 +1,13 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-class showPdf extends Controller
-{
-    public function pdfView($filename)
-{
+class showPdf extends Controller{
+    public function pdfView($filename){
     $path = '/public/contracts/' . $filename; // Adjust the path to match your storage structure
     if (Storage::disk('local')->exists($path)) {
-        $file = Storage::disk('local')->get($path);
+        $file = Storage::disk('public')->get($path);
         
         // Set the appropriate HTTP response headers
         $headers = [
