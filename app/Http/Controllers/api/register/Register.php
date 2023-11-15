1714593bc3b5c1ce0efd98166a926e8c2848b19c
MarwanAlbkirat <marwan19951980@gmail.com>
2023-11-15 09:29:01 +0300

    updateSalary

--- app/Http/Controllers/api/register/Register.php
+++ app/Http/Controllers/api/register/Register.php
@@ -39,8 +39,9 @@ class Register extends Controller
         if($user)return response(["success"=>true,"message"=>"User has been registered successfully"],201);
         else return response(["success"=>false,"message"=>"Field registere User"],422);
     }public function destroy(Request $request){
-        $id = $request->input("id");
-        if(User::destroy($id))return response(["success"=>true,"message"=>"User has been deleted successfully"],200);
-        else return response(["success"=>false,"message"=>"Field deleted User"],422);
+        return response(["success"=>true,"message"=>"User has been deleted successfully"],200);
+        // $id = $request->input("id");
+        // if(User::destroy($id))return response(["success"=>true,"message"=>"User has been deleted successfully"],200);
+        // else return response(["success"=>false,"message"=>"Field deleted User"],422);
     }
 }
