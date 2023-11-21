1f160bc8f3866b40237d0bc5128f1ea6648d1d84
MarwanAlbkirat <marwan19951980@gmail.com>
2023-11-21 14:42:57 +0300

    updateDashboard

--- app/Http/Livewire/Employees/VacationBalance.php
+++ app/Http/Livewire/Employees/VacationBalance.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Livewire\Employees;
 
+use App\Models\Company;
+use App\Models\Department;
+use App\Models\User;
 use Livewire\Component;
 
 class VacationBalance extends Component
 {
     public function render(){
-        return view('livewire.employees.vacation-balance');
+        $companies = Company::all();
+        $departments = Department::all();
+        $departments = Department::all();
+        $user = User::all();
+        return view('livewire.employees.vacation-balance',["users"=>$user, "companies"=> $companies,"departments"=> $departments]);
     }
 }
