03e7fefdacb25a2ad97cb7731dcdf9e04234354a
MarwanAlbkirat <marwan19951980@gmail.com>
2023-12-04 16:47:42 +0300

    update

--- resources/views/livewire/salaries/deposit-salary-pdf.blade.php
+++ resources/views/livewire/salaries/deposit-salary-pdf.blade.php
@@ -6,11 +6,11 @@
     <h5><span>الساده : {{$data["bank"]}}</span></h5>
     <h5><span>تحيه طيبه وبعد...........</span></h5>
     <h5><span>شركة :{{$data["company"]}}</span></h5>
-    <h5><span>رقم حسابنا لديكم :{{$data["account_number"]}}</span></h5>
-    <h5><span>بالاشاره الى الموضوع المذكور اعلاه  العمل على تحويل مالي </span></h5>
+    <h5><span>رقم حسابنا لديكم :  {{$data["account_number"]}}</span></h5>
+    <h5><span>بالاشاره الى الموضوع المذكور اعلاه  العمل على تحويل راتب </span></h5>
     <h5><span>الى حساباتهم الوارده  كما في جدول </span></h5>
     <h5><span>قيد القيمه   {{$data["salary"]}}  ,  {{$data["amount_written"]}} </span></h5>
-    <h5><span>وذالك  :    بدل راتب شهر ----      {{$data["month"]}}   </span></h5>
+    <h5><span>وذالك  :    بدل راتب شهر (  {{$data["month"]}} )  </span></h5>
     <h5><span>قيدا على حسابنا  جاري لديكم  المذكور اعلاه واشعارنا بذالك  . حسب الكشف التالي </span></h5>
     <table class="table table-light">
         <thead>
