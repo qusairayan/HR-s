de63d7a806c9d4bd4cec1aaad0f8fec7e6665378
MarwanAlbkirat <marwan19951980@gmail.com>
2024-03-12 13:38:51 +0300

    update

--- app/Http/Livewire/Salaries/NewSalary.php
+++ app/Http/Livewire/Salaries/NewSalary.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Livewire\Salaries;
 
 use App\Models\Allownce;
@@ -7,62 +8,67 @@ use App\Models\Deductions;
 use App\Models\Department;
 use App\Models\MonthlyPayroll;
 use App\Models\Promotion;
+use App\Models\SocialSecurity;
 use App\Models\User;
 use Carbon\Carbon;
 use Livewire\Component;
-class NewSalary extends Component{
-    public $user , $date ,$userSalary=0,$userDeduction=0,$userAllownces=0,$netSalary=0,$err="",$monthlyPayrollId;
-    public function render(){
-        $this->monthlyPayrollId = MonthlyPayroll::orderBy("id","desc")->pluck("id")->first();
-        $users = User::where("part_time",'')->orWhere("part_time",NULL)->get();
-        return view('livewire.salaries.new-salary',["monthlyPayrollId"=>$this->monthlyPayrollId,"users"=>$users,"userSalary"=>$this->userSalary,"userDeduction"=>$this->userDeduction,"userAllownces"=>$this->userAllownces,"netSalary"=>$this->netSalary]);
+
+class NewSalary extends Component
+{
+    public $user, $date, $userSalary = 0, $userDeduction = 0, $userAllownces = 0, $netSalary = 0, $err = "", $monthlyPayrollId;
+    public function render()
+    {
+        $this->monthlyPayrollId = MonthlyPayroll::orderBy("id", "desc")->pluck("id")->first();
+        $users = User::where("part_time", '')->orWhere("part_time", NULL)->get();
+        return view('livewire.salaries.new-salary', ["monthlyPayrollId" => $this->monthlyPayrollId, "users" => $users, "userSalary" => $this->userSalary, "userDeduction" => $this->userDeduction, "userAllownces" => $this->userAllownces, "netSalary" => $this->netSalary]);
     }
-    public function add(){
-        $this->userSalary = Promotion::where("user_id",$this->user)->where("from","<=",$this->date."-01")->orderBy("from","desc")->pluck("salary")->first();
-        $this->userDeduction = Deductions::where("user_id",$this->user)->where("date","LIKE",$this->date . '-%')->sum("amount");
-        $this->userAllownces = Allownce::where("user_id",$this->user)->where("date","LIKE",$this->date . '-%')->sum("amount");
-        $user = User::where("id",$this->user)->select("salary","start_date","unemployment_date")->get()->toArray();
-        if($user[0]["unemployment_date"])$unemployment = Carbon::parse( $user[0]["unemployment_date"]);
+    public function add()
+    {
+        $this->userSalary = Promotion::where("user_id", $this->user)->where("from", "<=", $this->date . "-01")->orderBy("from", "desc")->pluck("salary")->first();
+        $this->userDeduction = Deductions::where("user_id", $this->user)->where("date", "LIKE", $this->date . '-%')->sum("amount");
+        $this->userAllownces = Allownce::where("user_id", $this->user)->where("date", "LIKE", $this->date . '-%')->sum("amount");
+        $socilaSecurety = SocialSecurity::where("user_id", $this->user)->where("date", "LIKE", $this->date . '-%')->pluck("onEmployee")->first() ?? 0;
+        $user = User::where("id", $this->user)->select("salary", "start_date", "unemployment_date")->get()->toArray();
+        if ($user[0]["unemployment_date"]) $unemployment = Carbon::parse($user[0]["unemployment_date"]);
         else $unemployment = null;
-        $startDate = Carbon::parse( $user[0]["start_date"]);
-        if(!$this->userSalary){
+        $startDate = Carbon::parse($user[0]["start_date"]);
+        if (!$this->userSalary) {
             $this->userSalary = $user[0]["salary"];
         }
-        if($unemployment && $unemployment->format("Y-m") == $this->date){
+        if ($unemployment && $unemployment->format("Y-m") == $this->date) {
             $monthdays = 30;
-            if(explode("-",$this->date)[1] == 2){
-                $monthdays  = date("t") -1 ;
+            if (explode("-", $this->date)[1] == 2) {
+                $monthdays  = date("t") - 1;
             }
             $countDays = (int) $unemployment->format("d");
-            if($unemployment->format("Y-m")  == $startDate->format("Y-m")){
-                $countDays =  (int) $unemployment->format("d") - (int) $startDate->format("d") +1;
+            if ($unemployment->format("Y-m")  == $startDate->format("Y-m")) {
+                $countDays =  (int) $unemployment->format("d") - (int) $startDate->format("d") + 1;
             }
-            $salaryPerDay = $this->userSalary /$monthdays;
+            $salaryPerDay = $this->userSalary / $monthdays;
             $this->userSalary = $salaryPerDay * $countDays;
-            $this->userSalary =number_format($this->userSalary,2,"."," ");
-        }elseif($startDate->format("Y-m") === $this->date ){
+            $this->userSalary = number_format($this->userSalary, 2, ".", " ");
+        } elseif ($startDate->format("Y-m") === $this->date) {
             $countDays  = 31;
-            if(explode("-",$this->date)[1] == 2){
+            if (explode("-", $this->date)[1] == 2) {
                 $countDays  = $startDate->format("t");
                 $countDays++;
             }
             $countDays = $countDays - $startDate->format("d");
-            $salaryPerDay = $this->userSalary /30;
+            $salaryPerDay = $this->userSalary / 30;
             $this->userSalary = $salaryPerDay * $countDays;
-            $this->userSalary =number_format($this->userSalary,2,"."," ");
+            $this->userSalary = number_format($this->userSalary, 2, ".", " ");
         }
-       
-        $this->netSalary = $this->userSalary - $this->userDeduction + $this->userAllownces;
+        $this->netSalary = $this->userSalary - $this->userDeduction + $this->userAllownces - $socilaSecurety;
     }
-    public function approve(){
-        $monthlyPayroll = MonthlyPayroll::where("user_id",$this->user)->where("month","LIKE",$this->date."-%")->first();
-        
-        if($monthlyPayroll)return $this->err="The employee actually has a salary this month";
-        $data =new MonthlyPayroll();
-        $data->user_id=$this->user;
-        $data->salary=$this->netSalary;
-        $data->month=$this->date."-01";
-        $data =$data->save();
-        return $this->err="The employee's salary for this month has been successfully added";
+    public function approve()
+    {
+        $monthlyPayroll = MonthlyPayroll::where("user_id", $this->user)->where("month", "LIKE", $this->date . "-%")->first();
+        if ($monthlyPayroll) return $this->err = "The employee actually has a salary this month";
+        $data = new MonthlyPayroll();
+        $data->user_id = $this->user;
+        $data->salary = $this->netSalary;
+        $data->month = $this->date . "-01";
+        $data = $data->save();
+        return $this->err = "The employee's salary for this month has been successfully added";
     }
 }
--- app/Http/Livewire/Salaries/SocialSecurityController.php
+++ app/Http/Livewire/Salaries/SocialSecurityController.php
@@ -25,53 +25,49 @@ class SocialSecurityController extends Component
     public $search = '';
     public $company = '';
     public $department = '';
-    public $user='';
-    public $add=false;
-    public $salary="";
-    public $Netsalary="";
-    public $date="";
-   protected $rules = [
-        "salary"=>"required|integer",
-        "date"=>"required|date",
-        "user"=>"required"
+    public $user = '';
+    public $add = false;
+    public $salary = "";
+    public $Netsalary = "";
+    public $date = "";
+    protected $rules = [
+        "salary" => "required|integer",
+        "date" => "required|date",
+        "user" => "required"
     ];
     public function updated()
     {
         if ($this->user) {
-           $userSS= SocialSecurity::where('User_id','=',$this->user)->first();
+            $userSS = SocialSecurity::where('User_id', '=', $this->user)->first();
 
-           if($userSS){
-            $this->add=false;
-           }
-           else{
-            $this->add=true;
-            
+            if ($userSS) {
+                $this->add = false;
+            } else {
+                $this->add = true;
+            }
         }
-        }
-    
-
     }
 
-    public function addSocialSecurity(){
-        $exist=SocialSecurity::where('user_id','=',$this->user)->where("date","Like",date("Y-m")."-%")->first();
-        if(!$exist){
-
-            $userSalary = User::where('id',$this->user)->first()->only("salary");
+    public function addSocialSecurity()
+    {
+        $exist = SocialSecurity::where('user_id', '=', $this->user)->where("date", "Like", date("Y-m") . "-%")->first();
+        if (!$exist) {
+            $userSalary = User::where('id', $this->user)->first()->only("salary");
             $userSalary = $userSalary["salary"];
             $Salary_percentage = $this->salary / 100;
             $currentMonth = date('Y-m-d');
-            $socialsecurity=new SocialSecurity(
+            $socialsecurity = new SocialSecurity(
                 [
-                    'user_id'=>$this->user,
-                    'date'=>$this->date,
-                    'onEmployee'=> $Salary_percentage * SocialSecurityController::$EMPLOYEE_SOCIAL_SECURIT_DEDUCTION_RATE,
-                    'onCompany'=>$Salary_percentage* SocialSecurityController::$COMPANY_SOCIAL_SECURIT_DEDUCTION_RATE,
-                    "salary"=>$this->salary,
-                    "net_salary"=>$this->Netsalary
+                    'user_id' => $this->user,
+                    'date' => $this->date,
+                    'onEmployee' => $Salary_percentage * SocialSecurityController::$EMPLOYEE_SOCIAL_SECURIT_DEDUCTION_RATE,
+                    'onCompany' => $Salary_percentage * SocialSecurityController::$COMPANY_SOCIAL_SECURIT_DEDUCTION_RATE,
+                    "salary" => $this->salary,
+                    "net_salary" => $this->Netsalary
                 ]
-    );
-        $socialsecurity->save();
-        return redirect()->route("payrolls.socialsecurity");
+            );
+            $socialsecurity->save();
+            return redirect()->route("payrolls.socialsecurity");
         }
     }
     public function render()
@@ -80,54 +76,56 @@ class SocialSecurityController extends Component
         $companies = Company::all();
 
         $departmentsQuery = Department::select('*');
-        if($this->company){
-            $departmentsQuery-> Where('company_id', '=', $this->company);
+        if ($this->company) {
+            $departmentsQuery->Where('company_id', '=', $this->company);
         }
-        $departments=$departmentsQuery->get();
+        $departments = $departmentsQuery->get();
 
 
 
-        $usersQuery = User::where('social_security',1);
-        if($this->company){
+        $usersQuery = User::where('social_security', 1);
+        if ($this->company) {
             $usersQuery->Where('department_id', '=', $this->department);
         }
-        $users=$usersQuery->get();
+        $users = $usersQuery->get();
 
 
 
 
 
         $socialsecurityQuery = SocialSecurity::leftJoin('users', 'social_security.user_id', '=', 'users.id')
-        ->leftJoin('department', 'users.department_id', '=', 'department.id')
-        ->leftJoin('company', 'company.id', '=', 'users.company_id')
-        ->select('social_security.*', 'users.name as user_name','users.salary','users.position', 'department.name as dep_name','company.name as comp_name');
-        if($this->search){
-        $socialsecurityQuery->where('users.name', 'LIKE','%'. $this->search.'%')
-        ->orWhere('company.name', 'LIKE','%'. $this->search.'%')
-        ->orWhere('department.name', 'LIKE','%'. $this->search.'%');}
+            ->leftJoin('department', 'users.department_id', '=', 'department.id')
+            ->leftJoin('company', 'company.id', '=', 'users.company_id')
+            ->select('social_security.*', 'users.name as user_name', 'users.salary', 'users.position', 'department.name as dep_name', 'company.name as comp_name');
+        if ($this->search) {
+            $socialsecurityQuery->where('users.name', 'LIKE', '%' . $this->search . '%')
+                ->orWhere('company.name', 'LIKE', '%' . $this->search . '%')
+                ->orWhere('department.name', 'LIKE', '%' . $this->search . '%');
+        }
         if ($this->company) {
-            $socialsecurityQuery->where('users.company_id',$this->company );
+            $socialsecurityQuery->where('users.company_id', $this->company);
         }
         if ($this->department) {
-            $socialsecurityQuery->where('users.department_id',$this->department  ) ;
+            $socialsecurityQuery->where('users.department_id', $this->department);
         }
         if ($this->user) {
-            $socialsecurityQuery->where('users.id',$this->user  ) ;
+            $socialsecurityQuery->where('users.id', $this->user);
         }
-        $socialsecurity=$socialsecurityQuery->get();
-    
-foreach ($socialsecurity as $soc) {
-    $promotion=Promotion::where('user_id','=',$soc->user_id)->orderBy('from','desc')->first();
-    if ($promotion) {
-        $soc->salary=$promotion->salary;
-        $soc->position=$promotion->position;
-    }
-}
-        return view('livewire.salaries.socialsecurity', ['socialsecurity'=>$socialsecurity,'users'=>$users,'departments'=>$departments,'companies'=>$companies]);
+        $socialsecurity = $socialsecurityQuery->get();
+
+        foreach ($socialsecurity as $soc) {
+            $promotion = Promotion::where('user_id', '=', $soc->user_id)->orderBy('from', 'desc')->first();
+            if ($promotion) {
+                $soc->salary = $promotion->salary;
+                $soc->position = $promotion->position;
+            }
+        }
+        return view('livewire.salaries.socialsecurity', ['socialsecurity' => $socialsecurity, 'users' => $users, 'departments' => $departments, 'companies' => $companies]);
     }
 
 
-    public function add(){
+    public function add()
+    {
         $this->validate();
         $this->Netsalary = $this->salary - ($this->salary * 0.075);
     }
